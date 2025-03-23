951b9b986b9eec569773de94d3ab6c100b837d4d
An7u4n <miguelitocentu@hotmail.com>
2025-03-23 19:49:06 -0300

    feat: update RideCard and SearchSection components to use Trip type and fetch trips from API

--- app/components/RideCard.tsx
+++ app/components/RideCard.tsx
@@ -1,43 +1,29 @@
+import { Trip } from "@/types/trip";
 import { MapPin, Clock, User } from "lucide-react";
 
-interface RideCardProps { 
-    ride: {
-        driver: string;
-        car: string;
-        origin: string;
-        destination: string;
-        time: string;
-        availableSeats: number;
-    }
-}
-
-export default function RideCard({ride}: RideCardProps) {
-
+export default function RideCard({ trip }: { trip: Trip }) {
+  trip.startTime = new Date(trip.startTime);
   return (
     <div className="w-full max-w-2xl bg-white text-blue-400 shadow-xl rounded-2xl p-6 flex gap-6 items-center">
-      <div className="w-16 h-16 bg-blue-400 text-white font-bold flex items-center justify-center text-xl">
-        {ride.driver[0]}
+      <div className="w-16 h-16 rounded-full overflow-hidden bg-blue-400 text-white font-bold flex items-center justify-center text-xl">
+        {trip.driver?.profilePicture ? <img src={trip.driver?.profilePicture} alt={`Driver ${trip.driver?.name}`} /> : trip.driver?.name[0]}
       </div>
       <div className="flex-1">
-        <h2 className="text-2xl font-semibold">{ride.driver}</h2>
-        <p className="text-sm text-blue-400/80">{ride.car}</p>
+        <h2 className="text-2xl font-semibold">{trip.driver?.name}</h2>
+        <p className="text-sm text-blue-400/80">{`Car: ${trip.car?.brand} ${trip.car?.model} ${trip.car?.year}`}</p>
         <div className="mt-3 space-y-2">
           <div className="flex items-center gap-2">
             <MapPin className="w-5 h-5" />
-            <span>{ride.origin} → {ride.destination}</span>
-          </div>
+            <span>{trip.origin} → {trip.destination}</span>
+          </div>  
           <div className="flex items-center gap-2">
             <Clock className="w-5 h-5" />
-            <span>{ride.time}</span>
-          </div>
-          <div className="flex items-center gap-2">
-            <User className="w-5 h-5" />
-            <span>{ride.availableSeats} asientos disponibles</span>
+            <span>{`Trip starts ${trip.startTime.getDate().toString().padStart(2, '0')}-${trip.startTime.getMonth().toString().padStart(2, '0')}-${trip.startTime.getFullYear()} ${trip.startTime.getHours().toString().padStart(2, '0')}:${trip.startTime.getMinutes().toString().padStart(2, '0')}`}</span>
           </div>
         </div>
       </div>
-      <button className="bg-blue-400 text-white font-bold rounded-xl px-4 py-2">
-        Unirse al viaje
+      <button className="bg-blue-400 text-white font-bold rounded-xl px-4 py-2 cursor-pointer">
+        Get on Board!
       </button>
     </div>
   );
--- app/components/SearchSection.tsx
+++ app/components/SearchSection.tsx
@@ -1,33 +1,44 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchBarComponent from "./SearchBarComponent";
 import RideCard from "./RideCard";
+import { Trip } from "@/types/trip";
 
 export default function SearchSection(){
 
     const [searchData, setSearchData] = useState();
+    const [trips, setTrips] = useState<Trip[]>([]);
+    const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
     const handleSearch = (data: any) => {
-      console.log("Búsqueda enviada:", data);
       setSearchData(data);
     };
 
-    const ride = {
-        driver: "Juan Perez",
-        car: "Toyota Corolla 2018",
-        origin: "San Salvador",
-        destination: "Santa Ana",
-        time: "8:00 AM",
-        availableSeats: 3,
-    }
+    useEffect(() => {
+        fetch(`${API_URL}/trips`)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return res.json();
+            })
+            .then(data => {
+                console.log("API response:", data);
+                setTrips(data.trips)
+            })
+            .catch(err => console.error(err));
+    }, []);
+
 
     return (
         <div className="flex flex-col items-center justify-center gap-4 mt-[-200px]">
             <h1 className="text-4xl font-bold text-blue-400">Looking for a Ride?</h1>
-                <SearchBarComponent onSearch={handleSearch}/>
-
-            <div>
-                <RideCard ride={ride}  />
+            <SearchBarComponent onSearch={handleSearch}/>
+            <h3 className="self-start mt-4 text-2xl font-bold text-blue-400">Last Trips:</h3>
+            <div className="flex flex-col gap-8">
+                {trips.map((trip, index) => (
+                    <RideCard trip={trip} key={index} />
+                ))}
             </div>
         </div>
     )
