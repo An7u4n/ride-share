d50c3b9de85720a0f40aa65f42475f78a588d4ce
An7u4n <miguelitocentu@hotmail.com>
2025-04-03 20:20:11 -0300

    feat(trips): add GET endpoint to retrieve trip details by ID

---
+++ app/api/trips/[id]/route.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export const GET = async (request: NextRequest, { params }: { params: { id: string } }) => {
+    const { id } = params;
+
+    const trip = await prisma.trip.findFirst(
+        {
+            where: {
+                id: parseInt(id)
+            },
+            include: {
+                car: true,
+                driver: true,
+                users: true
+            }
+        },
+    )
+
+    if(!trip) return NextResponse.json({message: "Trip not found"}, {status: 404});
+
+    return NextResponse.json(trip, {status: 200});
+}
